87afc530cb9cd32c5ce7288bc7dc0df64208149f
erwinrudi <erwin@loyalto.id>
2022-10-12 14:53:48 +0700

    no message

--- src/index.js
+++ src/index.js
@@ -2,48 +2,93 @@ import EventEmitter from 'eventemitter3';
 import bs58 from 'bs58';
 
 let requestQueue = 1
-let handlerAdded = true
+let handlerAdded = false
 
 export default class Wallet extends EventEmitter {
     constructor() {
         super();
-        window.addEventListener('qoinpay_message_callback', this.#handleMessage);
+        window.addEventListener('qoinpay_message_callback', this.handleMessage);
     }
 
     connect = async () => {
-        return new Promise(resolve => {
-            this.#handleConnect();
+        return new Promise((resolve, reject) => {
+            this.#handleCall('connect');
             this.on('connect', (res) => {
-                resolve(res);
+                if (res.status === 1) {
+                    resolve(res.data)
+                }
+                else {
+                    reject(res.message)
+                }
             });
         });
     }
 
-    #handleConnect = async () => {
+    disconnect = async () => {
+        return new Promise((resolve, reject) => {
+            this.#handleCall('disconnect');
+            this.on('disconnect', (res) => {
+                if (res.status === 1) {
+                    resolve(res.message)
+                }
+                else {
+                    reject(res.message)
+                }
+            });
+        });
+    }
+
+    getAccount = async () => {
+        return new Promise((resolve, reject) => {
+            this.#handleCall('getAccount');
+            this.on('getAccount', (res) => {
+                if (res.status === 1) {
+                    resolve(res.data)
+                }
+                else {
+                    reject(res.message)
+                }
+            });
+        });
+    }
+
+
+
+
+    #handleCall = async (param) => {
         return new Promise((resolve) => {
-            this.#sendRequest('connect', {});
+            this.#sendRequest(param, {});
             resolve();
         });
     }
 
     #sendRequest = async (method, params) => {
-        const requestId = requestQueue
-        ++requestQueue;
-        if (method == "connect") {
-            window.qoinpay.connect({
+        if (window.qoinpay) {
+            const requestId = requestQueue
+            ++requestQueue;
+
+            window.qoinpay.call({
                 id: requestId,
-                method,
+                method: method,
                 params: {
                     ...params,
                 },
             });
         }
+        else {
+            this.emit(method, "Please Intall Qoinpay extension");
+        }
     }
 
-    #handleMessage = (e) => {
-        if (e.detail.method === 'connected') {
-            let address = e.detail.params.address
-            this.emit('connect', address);
+    handleMessage = (e) => {
+        if (e.detail.method === 'connect') {
+            this.emit(e.detail.method, e.detail.params);
+        }
+        else if (e.detail.method === 'getAccount') {
+            this.emit(e.detail.method, e.detail.params);
+        }
+        else if (e.detail.method === 'disconnect') {
+            this.emit(e.detail.method, e.detail.params);
         }
     }
 }
